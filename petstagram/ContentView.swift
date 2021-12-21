7203f2e9e7ba9e2d717a8f736d52d062aa7774d8
Paco Cardenal <paco@pacocardenal.com>
2021-12-21 15:29:07 +0100

    Initial commit

---
+++ petstagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  petstagram
+//
+//  Created by Paco Cardenal on 21/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ petstagram/petstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  petstagramApp.swift
+//  petstagram
+//
+//  Created by Paco Cardenal on 21/12/21.
+//
+
+import SwiftUI
+
+@main
+struct petstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
